769b6e2f137466ce350d0195f8284202951e3151
Mariia-Kovalenko <mariya.kovalenko.2000@gmail.com>
2020-10-13 14:28:44 +0300

    added classes

---
+++ src/com/company/BaseFactory.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class BaseFactory extends AbstractFactory {
+
+    @Override
+    public BaseFactory getFactory() {
+        return null;
+    }
+
+    public void createCircle(){
+
+    }
+    public void createTriangle(){
+
+    }
+}
---
+++ src/com/company/WhiteFactory.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public class WhiteFactory extends BaseFactory {
+
+}
